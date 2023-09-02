98e63ef028e6d81bf9d964d3c14acc64d5d66695
mingchiuli <mingchiuli@163.com>
2023-09-02 15:41:17 +0800

    20230902

--- megalith-frontend/src/utils/markdown.ts
+++ megalith-frontend/src/utils/markdown.ts
@@ -5,17 +5,17 @@ export const markdown = (mavonEditor: any, content: string): string => {
   return md.set(
     {
       highlight: (str: string, lang: string) => {
-        const codeIndex: number = Date.now() + Math.floor(Math.random() * 10000000);
-        let html: string = `<button style="background-color: white" class="copy-btn" type="button" data-clipboard-action="copy" data-clipboard-target="#copy${codeIndex}">copy</button>`;
+        const codeIndex = Date.now() + Math.floor(Math.random() * 10000000);
+        let html = `<button style="background-color: white" class="copy-btn" type="button" data-clipboard-action="copy" data-clipboard-target="#copy${codeIndex}">copy</button>`;
 
-        const linesLength: number = str.split(/\n/).length - 1;
-        let linesNum: string = '<span aria-hidden="true" class="line-numbers-rows">';
+        const linesLength = str.split(/\n/).length - 1;
+        let linesNum = '<span aria-hidden="true" class="line-numbers-rows">';
         for (let index = 0; index < linesLength; index++) {
           linesNum = linesNum + "<span></span>";
         }
         linesNum += "</span>";
         if (lang && hljs.getLanguage(lang)) {
-          const preCode: string = hljs.highlight(lang, str, true).value;
+          const preCode = hljs.highlight(lang, str, true).value;
           html = html + preCode;
           if (linesLength) {
             html += '<b class="name">' + lang + "</b>";
@@ -26,7 +26,7 @@ export const markdown = (mavonEditor: any, content: string): string => {
           )}</textarea>`
         }
 
-        const preCode: string = md.utils.escapeHtml(str);
+        const preCode = md.utils.escapeHtml(str);
         html = html + preCode;
         return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(
           /<\/textarea>/g,
