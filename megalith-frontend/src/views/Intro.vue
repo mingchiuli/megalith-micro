563a37e272e7ea8508121ecc457c0aab6fb3cb7b
mingchiuli <mingchiuli@163.com>
2023-11-05 02:23:45 +0800

    20231105

--- megalith-frontend/src/views/Intro.vue
+++ megalith-frontend/src/views/Intro.vue
@@ -17,7 +17,19 @@ const toGithub = () => window.location.href = 'https://github.com/mingchiuli'
     </div>
     <el-avatar shape="square" :size="200" :src=avatar fit="cover" />
     <el-card class="box-card" shadow="never">
-      <div v-for="o in 15" :key="o" class="text item">{{ 'List item ' + o }}</div>
+      <el-text size="large">
+        This is Adam Lea, a junior software developer who is majoring in Java.<br />
+        His short CV is following:<br />
+        <br />
+        2023-now, Boss Zhipin(NASDAQ:BZ), Software Engineering, Software Backend Programmer<br />
+        2020-2023, Jinan University, International Trade, Master of Economics degree<br />
+        2014-2018, Northwest A&F University, Grape and Wine Engineering, Bachelor of Engineering degree<br />
+        <br />
+        The technology stack used in the front-end of this website are Vue.js and Element Plus(A UI framework). I tried to write the front-end code with TypeScript.<br />
+        The technology stack used in the back-end of this website are Java and Spring.<br />
+        The program is deployed in Kubernetes.<br />
+        All codes for this program can be obtained in GitHub. If you want to get them, please click the left button.<br />
+      </el-text>
     </el-card>
     <Stat></Stat>
   </div>
