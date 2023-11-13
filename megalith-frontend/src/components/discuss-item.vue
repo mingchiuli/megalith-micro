b56fe5aa4f01f98edba2d27fb55a7e030fdf653b
mingchiuli <mingchiuli@163.com>
2023-11-13 22:27:02 +0800

    20231113

--- megalith-frontend/src/components/discuss-item.vue
+++ megalith-frontend/src/components/discuss-item.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import DisqusJS from 'disqusjs/dist/disqusjs.es2015.es.js'
 import 'disqusjs/dist/styles/disqusjs.css'
 import { onMounted, ref } from 'vue'
 
@@ -15,19 +14,12 @@ onMounted(() => {
   utterances.crossOrigin = 'anonymous'
   utterances.src = 'https://utteranc.es/client.js'
 
-  const disqus = new DisqusJS({
-    shortname: '81-68-192-120',
-    siteName: 'mingchiuli',
-    identifier: document.location.origin + document.location.pathname + document.location.search,
-    url: document.location.origin + document.location.pathname + document.location.search,
-    title: document.title,
-    api: 'https://disqus.skk.moe/disqus/',
-    apikey: 'dBfYxTiKx3J9iXzyiS000OB5a6Q2nk1dpCtXQZW3UnOKt0AoIvlqZ96bV2FsJiGp',
-    admin: 'mingchiuli'
-  })
-  // 是要插入评论的地方
-  disqus.render(document.getElementById('disqus')!)
-  document.getElementById('utterance')?.appendChild(utterances)
+  const disqus = document.createElement('script')
+  disqus.src = 'https://chiu-1.disqus.com/embed.js'
+  disqus.setAttribute('data-timestamp', new Date().toString())
+
+  document.getElementById('disqus_thread')!.appendChild(disqus)
+  document.getElementById('utterance')!.appendChild(utterances)
 })
 
 </script>
@@ -36,7 +28,7 @@ onMounted(() => {
   <div id="comment-div">
     <el-tabs :stretch=true v-model="activeName">
       <el-tab-pane label="Disqus" name="first">
-        <div id="disqus" style="width: inherit"></div>
+        <div id="disqus_thread" style="width: inherit"></div>
       </el-tab-pane>
       <el-tab-pane label="Utterance" name="second">
         <div id="utterance" style="width: inherit"></div>
