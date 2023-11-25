cea7a0c761c6573396c6efa8fd6f503e0f287402
mingchiuli <mingchiuli@163.com>
2023-11-25 10:45:46 +0800

    调整全量时机

--- megalith-frontend/src/views/sys/edit-view.vue
+++ megalith-frontend/src/views/sys/edit-view.vue
@@ -37,7 +37,6 @@ const client = new Client({
 const connect = () => {
   client.onConnect = _frame => {
     client.subscribe('/edits/push/all', _res => pushAllData())
-    pushAllData()
   }
 
   client.activate()
@@ -286,6 +285,7 @@ onUnmounted(() => {
     if (!client.connected) {
       ElNotification.warning("websocket reconnection ...")
       connect()
+      pushAllData()
     }
   }, 2000)
 })()
