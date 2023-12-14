d5e6ed331b5cb448407d6b7c25476547ad1ac7bc
mingchiuli <mingchiuli@163.com>
2023-12-14 23:34:15 +0800

    样式

--- megalith-frontend/src/views/blog-view.vue
+++ megalith-frontend/src/views/blog-view.vue
@@ -56,8 +56,8 @@ const catalogueRef = ref<InstanceType<typeof catalogue>>();
   } else if (width > catalogueWidth.value + 110) {
     right.value = 100
     showCatalogue.value = true
-  } else if (width > catalogueWidth.value + 10) {
-    right.value = 10
+  } else if (width > catalogueWidth.value + 5) {
+    right.value = 2.5
     showCatalogue.value = true
   } else {
     showCatalogue.value = false
