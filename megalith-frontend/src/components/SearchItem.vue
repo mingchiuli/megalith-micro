c1fe2e41e8c13055725103a010b076154e0c4d4b
mingchiuli <mingchiuli@163.com>
2024-03-23 21:28:10 +0800

    下拉框滚动加载添加

--- megalith-frontend/src/components/SearchItem.vue
+++ megalith-frontend/src/components/SearchItem.vue
@@ -3,7 +3,8 @@ import { GET } from '@/http/http'
 import router from '@/router'
 import type { BlogDesc, PageAdapter } from '@/type/entity'
 import type { ElAutocomplete } from 'element-plus'
-import { ref } from 'vue'
+import { onBeforeUnmount, ref, watch } from 'vue'
+import { debounce } from '@/utils/tools'
 
 const emit = defineEmits<{
   transSearchData: [payload: PageAdapter<BlogDesc>]
@@ -14,6 +15,8 @@ const year = defineModel<string>('year')
 const keywords = defineModel<string>('keywords')
 const loading = defineModel<boolean>('loading')
 const searchDialogVisible = defineModel<boolean>('searchDialogVisible')
+const suggestionList = ref<BlogDesc[]>([])
+let currentPage = 1
 
 const yearDialogVisible = ref(false)
 const search = async (queryString: string, currentPage: number, allInfo: boolean, year: string): Promise<PageAdapter<BlogDesc>> => {
@@ -23,21 +26,45 @@ const search = async (queryString: string, currentPage: number, allInfo: boolean
 }
 
 let timeout: NodeJS.Timeout
+let suggestionEle: Element | null
 const searchAbstractAsync = async (queryString: string, cb: Function) => {
   if (queryString.length) {
     //-1是后端一个默认参数
-    const page: PageAdapter<BlogDesc> = await search(queryString, -1, false, year.value!)
+    const page: PageAdapter<BlogDesc> = await search(queryString, currentPage, false, year.value!)
     page.content.forEach((blogsDesc: BlogDesc) => {
-      blogsDesc.value = ''
+      suggestionList.value.push(blogsDesc)
     })
     //节流
     clearTimeout(timeout)
     timeout = setTimeout(() => {
-      cb(page.content)
+      cb(suggestionList.value)
       if (!page.content.length) {
         ElMessage.error('No Records')
+        return
+      }
+      if (!suggestionEle) {
+        suggestionEle = document.querySelector('.select-list .el-autocomplete-suggestion__wrap')
+        suggestionEle?.addEventListener('scroll', debounce(() => load(suggestionEle!, cb)))
       }
     }, 1000 * Math.random())
+  }
+}
+
+watch(() => keywords.value, () => {
+  suggestionList.value.splice(0, suggestionList.value.length)
+})
+
+const load = async (e: Element, cb: Function) => {
+  if (keywords.value && e.scrollTop + e.clientHeight >= e.scrollHeight - 1) {
+    const page: PageAdapter<BlogDesc> = await search(keywords.value!, currentPage + 1, false, year.value!)
+    if (page.content.length === 0) return
+    if (page.content.length > 0) {
+      currentPage++
+    }
+    page.content.forEach((blogsDesc: BlogDesc) => {
+      suggestionList.value.push(blogsDesc)
+    })
+    cb(suggestionList.value)
   }
 }
 
@@ -77,7 +104,14 @@ const yearsCloseEvent = async () => {
   }
 }
 
-const clearSearch = () => keywords.value = ''
+const clearSearch = () => {
+  currentPage = 1
+  keywords.value = ''
+}
+
+onBeforeUnmount(() => {
+  suggestionEle?.removeEventListener('scroll', debounce(() => load))
+})
 
 defineExpose(
   { searchAllInfo }
@@ -92,8 +126,8 @@ defineExpose(
       <div class="dialog-year" v-if="year!.length">年份：{{ year }}</div>
       <div class="dialog-autocomplete">
         <el-autocomplete id="elc" v-model="keywords" :fetch-suggestions="searchAbstractAsync" placeholder="Please input"
-          @select="handleSelect" :trigger-on-focus="false" clearable @keyup.enter="searchAllInfo(keywords!)"
-          ref="refAutocomplete" @clear="clearSearch">
+          @select="handleSelect" :trigger-on-focus="false" popper-class="select-list" clearable
+          @keyup.enter="searchAllInfo(keywords!)" ref="refAutocomplete" @clear="clearSearch">
           <template #default="{ item }">
             <template v-if="item.highlight.title">
               <div class="value" v-for="(title, key) in item.highlight.title" v-bind:key="key" v-html="'标题：' + title" />
@@ -107,10 +141,16 @@ defineExpose(
                 v-html="'内容：' + content" />
             </template>
           </template>
+          <template #loading>
+            <svg class="circular" viewBox="0 0 50 50">
+              <circle class="path" cx="25" cy="25" r="20" fill="none" />
+            </svg>
+          </template>
         </el-autocomplete>
       </div>
 
-      <years-item v-model:year="year" v-model:yearDialogVisible="yearDialogVisible" @close="yearsCloseEvent"></years-item>
+      <years-item v-model:year="year" v-model:yearDialogVisible="yearDialogVisible"
+        @close="yearsCloseEvent"></years-item>
     </template>
     <template #footer>
       <div class="dialog-footer">
@@ -134,5 +174,21 @@ defineExpose(
 
 .dialog-hot {
   margin: 0 auto
+}
+
+.circular {
+  display: inline;
+  height: 30px;
+  width: 30px;
+  animation: loading-rotate 2s linear infinite
+}
+
+.path {
+  animation: loading-dash 1.5s ease-in-out infinite;
+  stroke-dasharray: 90, 150;
+  stroke-dashoffset: 0;
+  stroke-width: 2;
+  stroke: var(--el-color-primary);
+  stroke-linecap: round
 }
 </style>
--- megalith-frontend/src/type/entity.ts
+++ megalith-frontend/src/type/entity.ts
@@ -14,7 +14,6 @@ export interface BlogDesc {
   content?: string
   score?: number
   highlight?: SearchBlogsHighlightStruct,
-  value?: string
 }
 
 export interface BlogSys {
