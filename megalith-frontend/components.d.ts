907f0ab43a82f3bf3d2194bd0733d3d8f3241ee8
mingchiuli <mingchiuli@163.com>
2024-03-26 02:15:19 +0800

    ..

--- megalith-frontend/components.d.ts
+++ megalith-frontend/components.d.ts
@@ -50,7 +50,6 @@ declare module 'vue' {
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSkeleton: typeof import('element-plus/es')['ElSkeleton']
-    ElSkeletonItem: typeof import('element-plus/es')['ElSkeletonItem']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
