299cabc3b4f513c552b3b7e57e572aa5f4c22153
limingjiu <limingjiu@kanzhun.com>
2024-07-08 14:55:46 +0800

    remove

--- megalith-frontend/src/views/sys/EditView.vue
+++ megalith-frontend/src/views/sys/EditView.vue
@@ -448,7 +448,6 @@ const loadEditContent = async () => {
   form.userId = data.userId
   data.sensitiveContentList.forEach(item => sensitiveTags.value.push({ name: item, type: 'warning' }))
   sensitiveTags.value.forEach(item => form.sensitiveContentList.push(item.name))
-  console.log(form.sensitiveContentList)
   version = data.version
   if (fileList.value.length !== 0) {
     //移除全部
