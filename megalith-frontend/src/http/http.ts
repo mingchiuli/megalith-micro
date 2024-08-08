94bca243ebc1e1bea2e6dfb4263faa1f13b2660b
mingchiuli <mingchiuli@163.com>
2024-08-09 00:27:13 +0800

    log

--- megalith-frontend/src/http/http.ts
+++ megalith-frontend/src/http/http.ts
@@ -17,7 +17,7 @@ const DOWNLOAD_DATA = async (url: string, percentage: Ref<number>, percentageSho
       const { loaded, total } = progressEvent
       percentage.value = Math.floor((loaded * 100) / total!)
     },
-    responseType: 'blob',
+    // responseType: 'blob',
   }).then(res => {
     console.log(JSON.stringify(res))
     data = res
