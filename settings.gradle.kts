1db9bdbc00e22cd6cd434760b33b8eded368bf84
mingchiuli <mingchiuli@163.com>
2024-08-23 03:04:39 +0800

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,4 @@
 rootProject.name = "megalith-micro"
-ext.springboot.version = "3.3.2"
-ext.edisson.version = "3.35.0"
-ext.caffeine.version = "3.1.8"
-
 
 include("micro-gateway")
 include("micro-auth")
