c14835a4e8e68ed7a8fed4548ac4318a084b0d01
mingchiuli <mingchiuli@163.com>
2024-08-23 01:38:52 +0800

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
 rootProject.name = "megalith-micro"
 include("micro-gateway")
+include("micro-auth")
+include("micro-blog")
+include("micro-user")
+include("micro-search")
+include("micro-websocket")
+include("micro-exhibit")
