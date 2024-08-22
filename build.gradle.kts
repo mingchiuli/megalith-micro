a6190c6853c4ec73c74b274ce7aed317f71419cb
mingchiuli <mingchiuli@163.com>
2024-08-23 01:49:33 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,11 @@ plugins {
 }
 
 dependencies {
-	  implementation(project(":micro-gateway"))
+	implementation(project(":micro-gateway"))
+	implementation(project(":micro-auth"))
+	implementation(project(":micro-blog"))
+	implementation(project(":micro-user"))
+	implementation(project(":micro-exhibit"))
+	implementation(project(":micro-websocket"))
+	implementation(project(":micro-search"))
 }
