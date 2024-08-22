f69eaef865502dc4e267da9c2104984e020185f4
mingchiuli <mingchiuli@163.com>
2024-08-23 02:51:16 +0800

    test

--- micro-auth/build.gradle.kts
+++ micro-auth/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.redisson:redisson-spring-boot-starter:3.35.0")
+	implementation("org.redisson:redisson-spring-boot-starter:${redisson.version}")
 	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 	implementation("com.nimbusds:nimbus-jose-jwt:9.40")
 	compileOnly("org.projectlombok:lombok")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+rootProject.name = "megalith-micro"
+springboot.version = "3.3.2"
+redisson.version = "3.35.0"
+caffeine.version = "3.1.8"
+
+
 include("micro-gateway")
 include("micro-auth")
 include("micro-blog")
