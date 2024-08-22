ce07dbfec72941c9c81981f2406ee603653e38b5
mingchiuli <mingchiuli@163.com>
2024-08-23 02:22:45 +0800

    update

--- micro-auth/build.gradle.kts
+++ micro-auth/build.gradle.kts
@@ -23,19 +23,21 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-amqp")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.security:spring-security-crypto")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.redisson:redisson-spring-boot-starter:3.34.1")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+	implementation("com.nimbusds:nimbus-jose-jwt:9.40")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.amqp:spring-rabbit-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
@@ -49,7 +51,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 			"""
 				-march=compatibility
 				--gc=serial
-				-R:MaxHeapSize=256m
+				-R:MaxHeapSize=128m
 				-O2
 			""",
 		"BP_HEALTH_CHECKER_ENABLED" to "true"
@@ -57,7 +59,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 	docker {
 		publish.set(true)
 		publishRegistry {
-			imageName.set("docker.io/mingchiuli/micro-user:${version}")
+			imageName.set("docker.io/mingchiuli/micro-auth:${version}")
 			url.set("https://docker.io")
 			val un = System.getenv("DOCKER_USERNAME")
 			val pwd = System.getenv("DOCKER_PWD")
