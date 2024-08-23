70b6f41e7b10d746ee646887a583daf7105caebd
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-08-23 14:34:42 +0800

    Update build.gradle.kts

--- micro-auth/build.gradle.kts
+++ micro-auth/build.gradle.kts
@@ -9,6 +9,7 @@ configurations {
 	}
 }
 
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-security")
