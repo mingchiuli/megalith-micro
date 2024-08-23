67af4ed31ffaba83d67eebcbdca4e4b5924b183e
mingchiuli <mingchiuli@163.com>
2024-08-23 08:56:50 +0800

    update

--- micro-blog/build.gradle.kts
+++ micro-blog/build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("org.graalvm.buildtools.native") version "0.10.2"
 }
