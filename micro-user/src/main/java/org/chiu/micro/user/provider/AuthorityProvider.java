01cb55969dc4eec69237fdf040ba0a093fb1dc65
mingchiuli <mingchiuli@163.com>
2024-08-27 21:07:45 +0800

    inner interface

---
+++ micro-user/src/main/java/org/chiu/micro/user/provider/AuthorityProvider.java
@@ -0,0 +1,28 @@
+package org.chiu.micro.user.provider;
+
+import java.util.List;
+
+import org.chiu.micro.user.lang.Result;
+import org.chiu.micro.user.service.AuthorityService;
+import org.chiu.micro.user.vo.AuthorityVo;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/inner/authority")
+@Validated
+public class AuthorityProvider {
+
+    private final AuthorityService authorityService;
+
+    @PostMapping("/list")
+    public Result<List<AuthorityVo>> list() {
+        return Result.success(authorityService::findAll);
+    }
+
+}
