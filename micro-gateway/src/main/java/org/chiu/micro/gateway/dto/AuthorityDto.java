4a6a779bf07cac5ac305fc68d159e4f4a830288f
mingchiuli <mingchiuli@163.com>
2024-09-01 18:01:18 +0800

    update

--- micro-gateway/src/main/java/org/chiu/micro/gateway/dto/AuthorityDto.java
+++ micro-gateway/src/main/java/org/chiu/micro/gateway/dto/AuthorityDto.java
@@ -20,7 +20,9 @@ public class AuthorityDto {
 
     private String routePattern;
 
-    private String serviceName;
+    private String serviceHost;
+
+    private Integer servicePort;
     
     private Integer status;
 }
