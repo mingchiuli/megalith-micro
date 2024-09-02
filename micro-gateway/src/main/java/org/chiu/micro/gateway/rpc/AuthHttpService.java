3186bee2be9f030f86c5647c481cafe074c791a2
mingchiuli <mingchiuli@163.com>
2024-09-03 01:59:08 +0800

    update

--- micro-gateway/src/main/java/org/chiu/micro/gateway/rpc/AuthHttpService.java
+++ micro-gateway/src/main/java/org/chiu/micro/gateway/rpc/AuthHttpService.java
@@ -5,15 +5,16 @@ import java.util.List;
 import org.chiu.micro.gateway.dto.AuthDto;
 import org.chiu.micro.gateway.dto.AuthorityDto;
 import org.chiu.micro.gateway.lang.Result;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
 
 
 public interface AuthHttpService {
 
-    @GetExchange("/auth")
-    Result<AuthDto> getAuthentication(@RequestParam String token);
+    @GetExchange("/auth/{token}")
+    Result<AuthDto> getAuthentication(@PathVariable String token);
 
     @GetExchange("/auth/system")
-    Result<List<AuthorityDto>> getSystemAuthorities(@RequestParam List<String> service);
+    Result<List<AuthorityDto>> getSystemAuthorities(@RequestBody List<String> service);
 }
