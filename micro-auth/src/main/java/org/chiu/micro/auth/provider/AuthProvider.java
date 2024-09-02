cc168d547254e6eb1ff6f6533ff7d77028a25cab
mingchiuli <mingchiuli@163.com>
2024-09-03 01:59:08 +0800

    update

--- micro-auth/src/main/java/org/chiu/micro/auth/provider/AuthProvider.java
+++ micro-auth/src/main/java/org/chiu/micro/auth/provider/AuthProvider.java
@@ -9,7 +9,8 @@ import org.chiu.micro.auth.utils.SecurityAuthenticationUtils;
 import org.chiu.micro.auth.vo.AuthorityVo;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,14 +27,14 @@ public class AuthProvider {
 
     private final AuthService authService;
 
-    @GetMapping
+    @GetMapping("/{token}")
     @SneakyThrows
-    public Result<AuthDto> findById(@RequestParam String token) {
+    public Result<AuthDto> findById(@PathVariable String token) {
         return Result.success(() -> securityAuthenticationUtils.getAuthDto(token));
     }
 
     @GetMapping("/system")
-    public Result<List<AuthorityVo>> list(@RequestParam List<String> service) {
+    public Result<List<AuthorityVo>> list(@RequestBody List<String> service) {
         return Result.success(() -> authService.getSystemAuthority(service));
     }
 }
