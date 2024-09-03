3be636b7fc690517503747b5827a2b7fcd12ecfe
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-03 19:19:54 +0800

    Update AuthHttpService.java

--- micro-exhibit/src/main/java/org/chiu/micro/exhibit/rpc/AuthHttpService.java
+++ micro-exhibit/src/main/java/org/chiu/micro/exhibit/rpc/AuthHttpService.java
@@ -3,13 +3,13 @@ package org.chiu.micro.exhibit.rpc;
 import org.chiu.micro.exhibit.dto.AuthDto;
 import org.chiu.micro.exhibit.lang.Result;
 
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 
 
 public interface AuthHttpService {
 
-    @GetExchange("/auth")
-    Result<AuthDto> getAuthentication(@RequestParam String token);
+    @GetExchange("/auth/{token}")
+    Result<AuthDto> getAuthentication(@PathVariable String token);
 
 }
