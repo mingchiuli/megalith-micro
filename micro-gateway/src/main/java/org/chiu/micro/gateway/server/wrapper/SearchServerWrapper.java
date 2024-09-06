c88f9744c17fb64091a146a810a3af3193f92734
mingchiuli <mingchiuli@163.com>
2024-09-07 01:30:07 +0800

    update

--- micro-gateway/src/main/java/org/chiu/micro/gateway/server/wrapper/SearchServerWrapper.java
+++ micro-gateway/src/main/java/org/chiu/micro/gateway/server/wrapper/SearchServerWrapper.java
@@ -1,14 +1,11 @@
 package org.chiu.micro.gateway.server.wrapper;
 
 import org.chiu.micro.gateway.server.SearchServer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,14 +21,5 @@ public class SearchServerWrapper {
                                   @RequestParam(required = false) String year,
                                   @RequestParam String keywords) {
         return searchServer.selectBlogsByES(currentPage, allInfo, year, keywords);
-    }
-
-    @GetMapping("/sys/blogs")
-    @PreAuthorize("hasAuthority('sys:search:blogs')")
-    public byte[] searchAllBlogs(@RequestParam(required = false) Integer currentPage,
-                                 @RequestParam(required = false) Integer size,
-                                 @RequestParam String keywords,
-                                 HttpServletRequest request) {
-        return searchServer.searchAllBlogs(currentPage, size, keywords, request.getHeader(HttpHeaders.AUTHORIZATION));
     }
 }
