dc898b4155733ca38d7f5f80f275da8648c9327c
mingchiuli <mingchiuli@163.com>
2024-09-08 21:18:32 +0800

    update

--- micro-blog/build.gradle.kts
+++ micro-blog/build.gradle.kts
@@ -36,7 +36,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 				-march=compatibility
 				--gc=serial
 				-R:MaxHeapSize=512m
-				-O3
+				-Ob
 			""",
 		"BP_HEALTH_CHECKER_ENABLED" to "true"
 	)
