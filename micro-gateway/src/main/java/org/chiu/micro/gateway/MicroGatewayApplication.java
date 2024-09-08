e8c2ef7416b0e9076a0544f36a075ff5d050bcef
mingchiuli <mingchiuli@163.com>
2024-09-09 00:04:03 +0800

    update

--- micro-gateway/src/main/java/org/chiu/micro/gateway/MicroGatewayApplication.java
+++ micro-gateway/src/main/java/org/chiu/micro/gateway/MicroGatewayApplication.java
@@ -3,10 +3,9 @@ package org.chiu.micro.gateway;
 import org.chiu.micro.gateway.config.CustomRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
-@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class }, proxyBeanMethods = false)
+@SpringBootApplication(proxyBeanMethods = false)
 @ImportRuntimeHints({ CustomRuntimeHints.class })
 public class MicroGatewayApplication {
 
