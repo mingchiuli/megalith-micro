2693892f1c0be03ef1d4321f82de23a234e035c5
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-09 10:53:18 +0800

    Update AuthorityProvider.java

--- micro-user/src/main/java/org/chiu/micro/user/provider/AuthorityProvider.java
+++ micro-user/src/main/java/org/chiu/micro/user/provider/AuthorityProvider.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.chiu.micro.user.lang.Result;
 import org.chiu.micro.user.service.AuthorityService;
 import org.chiu.micro.user.vo.AuthorityVo;
+import org.chiu.micro.user.service.RoleAuthorityService;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +23,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthorityProvider {
 
     private final AuthorityService authorityService;
+    
+    private final RoleAuthorityService roleAuthorityService;
 
     @PostMapping("/list")
     public Result<List<AuthorityVo>> list(@RequestBody List<String> service) {
         return Result.success(() -> authorityService.findAllByService(service));
+    }
+
+    @GetMapping("/role/{rawRoles}")
+    Result<List<String>> getAuthoritiesByRoleCodes(@PathVariable String rawRoles) {
+        return Result.success(() -> roleAuthorityService.getAuthoritiesByRoleCodes(rawRoles));
     }
 
 }
