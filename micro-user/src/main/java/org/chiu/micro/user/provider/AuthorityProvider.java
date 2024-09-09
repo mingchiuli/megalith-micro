173c26d3041175473886da206b09f75e87da2015
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-09 10:56:56 +0800

    Update AuthorityProvider.java

--- micro-user/src/main/java/org/chiu/micro/user/provider/AuthorityProvider.java
+++ micro-user/src/main/java/org/chiu/micro/user/provider/AuthorityProvider.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
