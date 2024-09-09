87ac321b210e1e3f1ceceb2e45cab9337822026b
mingchiuli <mingchiuli@163.com>
2024-09-09 22:48:12 +0800

    update

--- micro-auth/build.gradle.kts
+++ micro-auth/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.redisson:redisson-spring-boot-starter:3.36.0")
+	implementation("org.redisson:redisson:3.36.0")
 	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 	implementation("com.nimbusds:nimbus-jose-jwt:9.40")
 	compileOnly("org.projectlombok:lombok")
--- micro-blog/src/main/java/org/chiu/micro/blog/exception/MissException.java
+++ micro-blog/src/main/java/org/chiu/micro/blog/exception/MissException.java
@@ -1,7 +1,4 @@
 package org.chiu.micro.blog.exception;
-
-import org.chiu.micro.blog.lang.ExceptionMessage;
-
 /**
  * @author mingchiuli
  * @create 2022-07-07 11:06 AM
