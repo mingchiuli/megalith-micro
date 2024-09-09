71b8ff813e4e8916aa54a79a1090b2cabd5b4ab6
mingchiuli <mingchiuli@163.com>
2024-09-10 00:24:05 +0800

    update

--- micro-websocket/src/main/java/org/chiu/micro/websocket/key/KeyFactory.java
+++ micro-websocket/src/main/java/org/chiu/micro/websocket/key/KeyFactory.java
@@ -16,7 +16,7 @@ public class KeyFactory {
 
     public static String createSubscriptionKey(Long userId, Long blogId) {
         return Objects.isNull(blogId) ?
-                "/init/" + userId.toString() :
+                "/init/" + userId :
                 "/" + blogId;
     }
 }
