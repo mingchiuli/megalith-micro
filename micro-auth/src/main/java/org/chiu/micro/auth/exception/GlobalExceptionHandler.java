21fc9f4896415b5bce900f8caaf9b5e24075c139
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-09 16:26:21 +0800

    Update GlobalExceptionHandler.java

--- micro-auth/src/main/java/org/chiu/micro/auth/exception/GlobalExceptionHandler.java
+++ micro-auth/src/main/java/org/chiu/micro/auth/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package org.chiu.micro.auth.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.chiu.micro.auth.lang.Result;
+
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
@@ -24,45 +26,45 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = BadCredentialsException.class)
     public String handler(BadCredentialsException e) {
-        return e.getMessage();
+        return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = AuthException.class)
     public String handler(AuthException e) {
-        return e.getMessage();
+        return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = BaseException.class)
     public String handler(BaseException e){
-        return e.getMessage();
+        return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public String handler(MethodArgumentNotValidException e) {
-        return e.getBindingResult().getAllErrors().stream()
+        return Result.fail(e.getBindingResult().getAllErrors().stream()
                 .findFirst()
-                .<String>map(MessageSourceResolvable::getDefaultMessage).orElse("");
+                .<String>map(MessageSourceResolvable::getDefaultMessage).orElse(""));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = IllegalArgumentException.class)
     public String handler(IllegalArgumentException e) {
-        return e.getMessage();
+        return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(value = AccessDeniedException.class)
     public String handler(AccessDeniedException e){
-        return e.getMessage();
+        return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = RuntimeException.class)
     public String handler(RuntimeException e) {
-        return e.getMessage();
+        return Result.fail(e.getMessage());
     }
 
 }
