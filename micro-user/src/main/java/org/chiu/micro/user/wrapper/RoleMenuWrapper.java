54c4710dcc33486e48865c8bfbed4e64e38f337b
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-11 19:53:34 +0800

    Update RoleMenuWrapper.java

--- micro-user/src/main/java/org/chiu/micro/user/wrapper/RoleMenuWrapper.java
+++ micro-user/src/main/java/org/chiu/micro/user/wrapper/RoleMenuWrapper.java
@@ -22,6 +22,7 @@ public class RoleMenuWrapper {
     @Transactional
     public void saveMenu(Long roleId, List<RoleMenuEntity> roleMenuEntities) {
         roleMenuRepository.deleteByRoleId(roleId);
+        roleMenuRepository.flush();
         roleMenuRepository.saveAll(roleMenuEntities);
     }
 
