3f1b82020b21883c30e2df4f1a699be76fb8b0fe
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-12 13:49:39 +0800

    Update AuthProvider.java

--- micro-auth/src/main/java/org/chiu/micro/auth/provider/AuthProvider.java
+++ micro-auth/src/main/java/org/chiu/micro/auth/provider/AuthProvider.java
@@ -10,12 +10,10 @@ import org.chiu.micro.auth.service.AuthService;
 import org.chiu.micro.auth.utils.SecurityAuthenticationUtils;
 import org.chiu.micro.auth.vo.AuthorityRouteVo;
 import org.chiu.micro.auth.vo.AuthorityVo;
-import org.springframework.http.HttpHeaders;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
