cf5604a9c5e9325a42b79768768c3b4eb77b86f3
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-09-12 13:41:53 +0800

    Update AuthHttpServiceWrapper.java

--- micro-gateway/src/main/java/org/chiu/micro/gateway/rpc/wrapper/AuthHttpServiceWrapper.java
+++ micro-gateway/src/main/java/org/chiu/micro/gateway/rpc/wrapper/AuthHttpServiceWrapper.java
@@ -7,8 +7,6 @@ import org.chiu.micro.gateway.req.AuthorityRouteReq;
 import org.chiu.micro.gateway.rpc.AuthHttpService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +20,7 @@ public class AuthHttpServiceWrapper {
     private final AuthHttpService authHttpService;
 
     public AuthorityRouteDto getAuthorityRoute(AuthorityRouteReq req) {
-        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
-        Result<AuthorityRouteDto> result = authHttpService.getAuthorityRoute(req, request.getHeader(HttpHeaders.AUTHORIZATION));
+        Result<AuthorityRouteDto> result = authHttpService.getAuthorityRoute(req);
         if (result.getCode() != 200) {
             throw new MissException(result.getMsg());
         }
