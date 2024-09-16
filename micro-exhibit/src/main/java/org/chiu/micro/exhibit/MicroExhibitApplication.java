da5658dcfdbaf01afe16407710c3c249268caabb
mingchiuli <mingchiuli@163.com>
2024-09-16 23:27:52 +0800

    update

--- micro-exhibit/src/main/java/org/chiu/micro/exhibit/MicroExhibitApplication.java
+++ micro-exhibit/src/main/java/org/chiu/micro/exhibit/MicroExhibitApplication.java
@@ -4,12 +4,10 @@ import org.chiu.micro.exhibit.config.CustomRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(proxyBeanMethods = false)
-@EnableAsync
 @EnableScheduling
 @ImportRuntimeHints({ CustomRuntimeHints.class })
 public class MicroExhibitApplication {
