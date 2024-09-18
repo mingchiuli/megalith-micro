f32b4ad572337a3c7b658e40c7a910c6ffff8695
mingchiuli <mingchiuli@163.com>
2024-09-18 23:31:13 +0800

    remove spring redis

--- micro-auth/src/main/java/org/chiu/micro/auth/page/PageAdapter.java
+++ micro-auth/src/main/java/org/chiu/micro/auth/page/PageAdapter.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -33,17 +32,5 @@ public class PageAdapter<T> {
     private boolean empty;
 
     private int totalPages;
-
-    //只用把原来的page类放进来即可
-    public PageAdapter(Page<T> page) {
-        this.content = page.getContent();
-        this.totalElements = page.getTotalElements();
-        this.pageNumber = page.getPageable().getPageNumber() + 1;
-        this.pageSize = page.getPageable().getPageSize();
-        this.first = page.isFirst();
-        this.last = page.isLast();
-        this.empty = page.isEmpty();
-        this.totalPages = page.getTotalPages();
-    }
 
 }
