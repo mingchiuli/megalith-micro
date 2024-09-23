7b7abb7d89d49a22145791f01d011e8c1b1ee040
mingchiuli <mingchiuli@163.com>
2024-09-24 01:02:41 +0800

    test

--- micro-user/build.gradle.kts
+++ micro-user/build.gradle.kts
@@ -32,7 +32,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 				-O3
 			""",
 		"BP_HEALTH_CHECKER_ENABLED" to "true",
-		"JAVA_TOOL_OPTIONS" to "-Xmx12G -Xms12G"
+		"JAVA_TOOL_OPTIONS" to "-Xmx11G -Xms11G"
 	)
 	docker {
 		publish.set(true)
