13a53d9fe3c86598bdf58928bf11378864348ce2
mingchiuli <mingchiuli@163.com>
2024-09-26 21:50:35 +0800

    remove stomp

--- micro-websocket/src/main/java/org/chiu/micro/websocket/MicroWebSocketApplication.java
+++ micro-websocket/src/main/java/org/chiu/micro/websocket/MicroWebSocketApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class }, proxyBeanMethods = false)
 @ImportRuntimeHints({ CustomRuntimeHints.class })
-@EnableWebSocketSecurity
 @EnableWebSocketMessageBroker
 public class MicroWebSocketApplication {
 
