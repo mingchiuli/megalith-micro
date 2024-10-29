6f3ba42b05ce112ec1679819f3473e4fb4067d62
mingchiuli <mingchiuli@163.com>
2024-10-30 00:30:31 +0800

    release package

--- cache/build.gradle.kts
+++ cache/build.gradle.kts
@@ -1,5 +1,44 @@
 
-version = "1.0"
+import com.vanniktech.maven.publish.SonatypeHost
+
+plugins {
+    id("com.vanniktech.maven.publish") version "0.30.0"
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("$group", "cache-spring-boot-starter", "$version")
+
+    pom {
+        name.set("Megalith Cache")
+        description.set("A Cache Framework for Megalith")
+        inceptionYear.set("2024")
+        url.set("https://github.com/mingchiuli/megalith-micro/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("chiu")
+                name.set("mingchiuli")
+                url.set("https://github.com/mingchiuli/")
+            }
+        }
+        scm {
+            url.set("https://github.com/mingchiuli/megalith-micro/")
+            connection.set("scm:git:git://github.com/mingchiuli/megalith-micro.git")
+            developerConnection.set("scm:git:ssh://git@github.com/mingchiuli/megalith-micro.git")
+        }
+    }
+}
+
+version = "1.0.0"
 
 sourceSets {
     create("rabbitmqSupport") {
