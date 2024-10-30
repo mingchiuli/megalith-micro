30ee62b049c4738d3910667b552b854255c3d9d1
mingchiuli <mingchiuli@163.com>
2024-10-30 08:48:02 +0800

    use own package

--- cache/build.gradle.kts
+++ cache/build.gradle.kts
@@ -50,12 +50,9 @@ sourceSets {
 }
 
 java {
-    sourceSets {
-        val rabbitmqSupport by getting {
-            dependencies {
-                implementation("org.springframework.amqp:spring-rabbit")
-            }
-        }
+    registerFeature("rabbitmqSupport") {
+        usingSourceSet(sourceSets["rabbitmqSupport"])
+        capability("$group", "cache-rabbit-support", "$version")
     }
 }
 
@@ -63,6 +60,9 @@ dependencies {
     implementation("org.redisson:redisson:${ext.get("redisson.version")}")
     implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("org.aspectj:aspectjweaver")
+    sourceSets.named("rabbitmqSupport") {
+        implementation("org.springframework.amqp:spring-rabbit")
+    }
     compileOnly("jakarta.servlet:jakarta.servlet-api")
     compileOnly("org.springframework:spring-context")
     compileOnly("org.springframework:spring-core")
