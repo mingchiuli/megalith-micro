0690195854db14da07018e32b50e2e81fc478ea3
mingchiuli <mingchiuli@163.com>
2024-11-01 09:04:08 +0800

    fix

--- cache/build.gradle.kts
+++ cache/build.gradle.kts
@@ -1,7 +1,7 @@
 
 import com.vanniktech.maven.publish.SonatypeHost
 
-version = "1.1.0"
+version = "1.2.0"
 
 plugins {
     id("com.vanniktech.maven.publish") version "0.30.0"
--- cache/src/main/java/wiki/chiu/micro/cache/config/JsonConverterConfig.java
+++ cache/src/main/java/wiki/chiu/micro/cache/config/JsonConverterConfig.java
@@ -1,8 +1,6 @@
 package wiki.chiu.micro.cache.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import wiki.chiu.micro.cache.utils.CommonCacheKeyGenerator;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -15,19 +13,8 @@ import org.springframework.context.annotation.Bean;
 @ConditionalOnProperty(prefix = "megalith", name = "cache.topic", havingValue = "rabbit")
 public class JsonConverterConfig {
 
-    private final ObjectMapper objectMapper;
-
-    public JsonConverterConfig(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @Bean(name = "cacheMessageConverter")
     Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    CommonCacheKeyGenerator commonCacheKeyGenerator() {
-        return new CommonCacheKeyGenerator(objectMapper);
     }
 }
