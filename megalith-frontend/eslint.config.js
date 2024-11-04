0ca0161b65e449c921390aaf585066429e46c4b3
mingchiuli <mingchiuli@163.com>
2024-11-04 23:05:59 +0800

    eslint add

---
+++ megalith-frontend/eslint.config.js
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"]
+  },
+  {
+    languageOptions: {
+      globals: globals.browser 
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"], languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser
+      }
+    }
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-undef": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off"
+    }
+  }
+];
