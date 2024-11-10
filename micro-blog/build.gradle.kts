b64fc1e4fb7d0acf8c47365406e05831e7713255
mingchiuli <mingchiuli@163.com>
2024-11-10 12:31:13 +0800

    limit memory

--- micro-blog/build.gradle.kts
+++ micro-blog/build.gradle.kts
@@ -26,7 +26,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 			"""
 				-march=compatibility
 				--gc=serial
-				-R:MaxHeapSize=512m
+				-R:MaxHeapSize=256m
 				-O3
 				-J-XX:MaxRAMPercentage=80.0
 				-H:+CompactingOldGen
