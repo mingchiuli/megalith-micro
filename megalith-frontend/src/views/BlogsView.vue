b89e29327abd57f6f038ca0099409c280b9a8552
mingchiuli <mingchiuli@163.com>
2024-11-17 17:29:20 +0800

    fix

--- megalith-frontend/src/views/BlogsView.vue
+++ megalith-frontend/src/views/BlogsView.vue
@@ -147,7 +147,7 @@ const { content, totalElements, pageSize } = toRefs(page)
       "
       >进入后台</el-link
     >
-    <el-link type="warning" v-if="keywords" link @click="refresh">返回</el-link>
+    <el-link class="door" type="warning" v-if="keywords" link @click="refresh">返回</el-link>
     <br />
     <div class="description">
       <el-skeleton animated :loading="loading" :throttle="300">
