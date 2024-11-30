52d0acee9c586dcf0ae030244b4c32dac4962a5b
mingchiuli <mingchiuli@163.com>
2024-12-01 01:36:30 +0800

    update

--- micro-auth/build.gradle.kts
+++ micro-auth/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 	implementation("org.redisson:redisson:3.39.0")
 	implementation("com.nimbusds:nimbus-jose-jwt:9.47")
 	implementation(project(":common"))
-	implementation("wiki.chiu.megalith:cache-spring-boot-starter:3.3.5-FINAL")
+	implementation("wiki.chiu.megalith:cache-spring-boot-starter:3.3.5-FINAL-Z")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
