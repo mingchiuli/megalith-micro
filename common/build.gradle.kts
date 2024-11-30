878b1649e689065d5b6d504c2ce28179372da1b7
mingchiuli <mingchiuli@163.com>
2024-12-01 00:48:42 +0800

    remove

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -5,6 +5,5 @@ dependencies {
     compileOnly("org.springframework:spring-web")
     compileOnly("org.springframework:spring-context")
     compileOnly("com.fasterxml.jackson.core:jackson-databind")
-    compileOnly("jakarta.validation:jakarta.validation-api")
     compileOnly("jakarta.servlet:jakarta.servlet-api")
 }
