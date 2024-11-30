ccd0ef2eba2f49f4dfc752c5dd696f3f98fc1b4b
mingchiuli <mingchiuli@163.com>
2024-12-01 01:36:30 +0800

    update

--- micro-exhibit/build.gradle.kts
+++ micro-exhibit/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation(project(":common"))
-	implementation("wiki.chiu.megalith:cache-spring-boot-starter:3.3.5-FINAL")
+	implementation("wiki.chiu.megalith:cache-spring-boot-starter:3.3.5-FINAL-Z")
 	implementation("org.redisson:redisson:3.39.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
