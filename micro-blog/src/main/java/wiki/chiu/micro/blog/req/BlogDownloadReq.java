ed2d1bd02b35f0619fdbe49909e1c78cba5ee296
mingchiuli <mingchiuli@163.com>
2024-12-03 12:29:22 +0800

    opt

---
+++ micro-blog/src/main/java/wiki/chiu/micro/blog/req/BlogDownloadReq.java
@@ -0,0 +1,17 @@
+package wiki.chiu.micro.blog.req;
+
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+public record BlogDownloadReq(
+        @Size(max = 20)
+        String keywords,
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime createStart,
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime createEnd) {
+}
