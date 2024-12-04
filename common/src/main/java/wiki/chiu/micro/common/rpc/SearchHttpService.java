2d69097518b1dea6d2a9461b1882bb8b70c76f88
mingchiuli <mingchiuli@163.com>
2024-12-04 23:36:14 +0800

    opt

--- common/src/main/java/wiki/chiu/micro/common/rpc/SearchHttpService.java
+++ common/src/main/java/wiki/chiu/micro/common/rpc/SearchHttpService.java
@@ -4,6 +4,7 @@ package wiki.chiu.micro.common.rpc;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 import wiki.chiu.micro.common.dto.BlogSearchRpcDto;
 import wiki.chiu.micro.common.lang.Result;
@@ -12,8 +13,8 @@ import wiki.chiu.micro.common.req.BlogSysSearchReq;
 
 public interface SearchHttpService {
 
-    @PostExchange("/blog/search")
-    Result<BlogSearchRpcDto> searchBlogs(@RequestBody BlogSysSearchReq req);
+    @GetExchange("/blog/search")
+    Result<BlogSearchRpcDto> searchBlogs(BlogSysSearchReq req);
 
     @PostExchange("/blog/count")
     Result<Long> countBlogs(@RequestBody BlogSysCountSearchReq req);
