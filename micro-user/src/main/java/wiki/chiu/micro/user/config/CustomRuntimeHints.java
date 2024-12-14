7ed35bb9b335290c3a0de8c789ee4759a7cc23f1
mingchiuli <mingchiuli@163.com>
2024-12-14 11:05:37 +0800

    remove

--- micro-user/src/main/java/wiki/chiu/micro/user/config/CustomRuntimeHints.java
+++ micro-user/src/main/java/wiki/chiu/micro/user/config/CustomRuntimeHints.java
@@ -1,17 +1,13 @@
 package wiki.chiu.micro.user.config;
 
-import wiki.chiu.micro.common.exception.MissException;
 import wiki.chiu.micro.user.constant.UserAuthMenuOperateMessage;
-import wiki.chiu.micro.user.valid.impl.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
-import java.util.LinkedHashSet;
 
 
 public class CustomRuntimeHints implements RuntimeHintsRegistrar {
@@ -21,13 +17,6 @@ public class CustomRuntimeHints implements RuntimeHintsRegistrar {
     @Override// Register method for reflection
     public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
         // Register method for reflection
-
-        try {
-            hints.reflection().registerConstructor(LinkedHashSet.class.getDeclaredConstructor(), ExecutableMode.INVOKE);
-        } catch (NoSuchMethodException e) {
-            log.error(e.getMessage());
-            throw new MissException(e.getMessage());
-        }
 
         hints.serialization().registerType(UserAuthMenuOperateMessage.class);
 
