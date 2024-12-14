5a2b3bbd396c06052e1a3824b9a4ef2d21439ac4
mingchiuli <mingchiuli@163.com>
2024-12-14 11:05:37 +0800

    remove

--- micro-blog/src/main/java/wiki/chiu/micro/blog/config/CustomRuntimeHints.java
+++ micro-blog/src/main/java/wiki/chiu/micro/blog/config/CustomRuntimeHints.java
@@ -4,27 +4,18 @@ import wiki.chiu.micro.blog.constant.BlogOperateMessage;
 import wiki.chiu.micro.blog.dto.BlogDeleteDto;
 import wiki.chiu.micro.blog.req.BlogDownloadReq;
 import wiki.chiu.micro.blog.req.BlogQueryReq;
-import wiki.chiu.micro.blog.valid.impl.*;
 
-import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
-import java.util.LinkedHashSet;
 
 public class CustomRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override// Register method for reflection
     public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
         // Register method for reflection
-
-        try {
-            hints.reflection().registerConstructor(LinkedHashSet.class.getDeclaredConstructor(), ExecutableMode.INVOKE);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("application start fail");
-        }
 
         hints.serialization().registerType(BlogOperateMessage.class);
         hints.serialization().registerType(BlogQueryReq.class);
