2880ddd1cba742938257d82a036e72e434f3b359
mingchiuli <mingchiuli@163.com>
2024-12-18 20:28:52 +0800

    opt package

--- cache/src/main/java/wiki/chiu/micro/cache/annotation/Checker.java
+++ cache/src/main/java/wiki/chiu/micro/cache/annotation/Checker.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
 
 /**
  * @author mingchiuli
- * @create 2022-06-07 11:00 AM
+ * @since 2022-06-07 11:00 AM
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
