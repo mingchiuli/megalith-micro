6c052068437bbd0d24b9dbc1707c023bb5f34c5f
mingchiuli <mingchiuli@163.com>
2024-12-20 21:27:20 +0800

    opt gateway service

--- micro-gateway/src/main/java/wiki/chiu/micro/gateway/rpc/AuthHttpServiceWrapper.java
+++ micro-gateway/src/main/java/wiki/chiu/micro/gateway/rpc/AuthHttpServiceWrapper.java
@@ -2,12 +2,12 @@ package wiki.chiu.micro.gateway.rpc;
 
 import wiki.chiu.micro.common.dto.AuthorityRouteRpcDto;
 
-import wiki.chiu.micro.common.exception.MissException;
-import wiki.chiu.micro.common.lang.Result;
 
 import wiki.chiu.micro.common.req.AuthorityRouteReq;
 import wiki.chiu.micro.common.rpc.AuthHttpService;
 import org.springframework.stereotype.Component;
+
+import static wiki.chiu.micro.common.lang.Result.handleResult;
 
 @Component
 public class AuthHttpServiceWrapper {
@@ -19,11 +19,7 @@ public class AuthHttpServiceWrapper {
     }
 
     public AuthorityRouteRpcDto getAuthorityRoute(AuthorityRouteReq req) {
-        Result<AuthorityRouteRpcDto> result = authHttpService.getAuthorityRoute(req);
-        if (result.code() != 200) {
-            throw new MissException(result.msg());
-        }
-        return result.data();
+        return handleResult(() -> authHttpService.getAuthorityRoute(req));
     }
 
 }
