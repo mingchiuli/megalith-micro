4f1579eddb6327eb99fdf25635d157cd7669b1dc
mingchiuli <mingchiuli@163.com>
2025-01-13 00:03:28 +0800

    opt frontend

--- micro-auth/src/main/java/wiki/chiu/micro/auth/convertor/MenuDisplayDtoConvertor.java
+++ micro-auth/src/main/java/wiki/chiu/micro/auth/convertor/MenuDisplayDtoConvertor.java
@@ -2,8 +2,6 @@ package wiki.chiu.micro.auth.convertor;
 
 import wiki.chiu.micro.auth.dto.MenuDisplayDto;
 import wiki.chiu.micro.auth.dto.MenuDto;
-import wiki.chiu.micro.common.exception.MissException;
-import wiki.chiu.micro.common.lang.ExceptionMessage;
 import wiki.chiu.micro.common.lang.StatusEnum;
 
 import java.util.Comparator;
@@ -37,12 +35,8 @@ public class MenuDisplayDtoConvertor {
     public static List<MenuDisplayDto> buildTreeMenu(List<MenuDisplayDto> menus) {
         //2.组装父子的树形结构
         //2.1 找到所有一级分类
-        MenuDisplayDto root = menus.stream()
-                .filter(menu -> Objects.equals(menu.parentId(), 0L)).findFirst()
-                .orElseThrow(() -> new MissException(ExceptionMessage.NO_FOUND));
-
         return menus.stream()
-                .filter(menu -> Objects.equals(menu.parentId(), root.menuId()))
+                .filter(menu -> menu.parentId() == 0)
                 .map(menu -> new MenuDisplayDto(menu, 0L, getChildren(menu, menus)))
                 .sorted(Comparator.comparingInt(menu -> Objects.isNull(menu.orderNum()) ? 0 : menu.orderNum()))
                 .toList();
