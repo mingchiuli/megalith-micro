32c17b9f10da8e7cc2861b1c771dd0254bf74cf3
chiu <80095372+mingchiuli@users.noreply.github.com>
2025-01-17 00:25:45 +0800

    Update RolesView.vue

--- megalith-frontend/src/views/sys/RolesView.vue
+++ megalith-frontend/src/views/sys/RolesView.vue
@@ -279,7 +279,7 @@ const handleDelete = async (row: RoleSys) => {
       </template>
     </el-table-column>
 
-    <el-table-column :fixed="fix" label="操作" min-width="260" align="center">
+    <el-table-column :fixed="fix" label="操作" min-width="250" align="center">
       <template #default="scope">
         <template v-if="checkButtonAuth(ButtonAuth.SYS_ROLE_EDIT)">
           <el-button
