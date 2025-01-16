3cfcf3c7d886eb5a7cd71a0983aeffdd1075e786
chiu <80095372+mingchiuli@users.noreply.github.com>
2025-01-16 09:32:41 +0800

    Update DeleteMenu.java

--- micro-user/src/main/java/wiki/chiu/micro/user/valid/DeleteMenu.java
+++ micro-user/src/main/java/wiki/chiu/micro/user/valid/DeleteMenu.java
@@ -2,7 +2,7 @@ package wiki.chiu.micro.user.valid;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import wiki.chiu.micro.user.valid.impl.MenuValueConstraintValidator;
+import wiki.chiu.micro.user.valid.impl.DeleteMenuConstraintValidator;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -12,7 +12,7 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = {MenuValueConstraintValidator.class})
+@Constraint(validatedBy = {DeleteMenuConstraintValidator.class})
 @Target({ PARAMETER })
 @Retention(RUNTIME)
 public @interface DeleteMenu {
