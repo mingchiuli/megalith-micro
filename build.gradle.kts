32c618b3bfd655554e19ad69e969178562ad3c7c
mingchiuli <mingchiuli@163.com>
2025-01-18 21:44:53 +0800

    update pkg

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,9 @@ subprojects {
 	dependencyManagement {
 		dependencies {
 			dependency("com.github.ben-manes.caffeine:caffeine:3.2.0")
-			dependency("org.redisson:redisson:3.34.0")
+			dependency("org.redisson:redisson:3.43.0")
 			dependency("com.nimbusds:nimbus-jose-jwt:10.0.1")
-			dependency("wiki.chiu.megalith:cache-spring-boot-starter:3.4.1.9")
+			dependency("wiki.chiu.megalith:cache-spring-boot-starter:3.4.1.10")
 		}
 	}
 
