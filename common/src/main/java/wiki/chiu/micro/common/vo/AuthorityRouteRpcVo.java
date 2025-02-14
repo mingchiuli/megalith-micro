6b01abea589e81ea2163488585ca1ad137b7cb9f
mingchiuli <mingchiuli@163.com>
2025-02-15 01:13:30 +0800

    opt code

--- common/src/main/java/wiki/chiu/micro/common/vo/AuthorityRouteRpcVo.java
+++ common/src/main/java/wiki/chiu/micro/common/vo/AuthorityRouteRpcVo.java
@@ -6,4 +6,27 @@ public record AuthorityRouteRpcVo(
         String serviceHost,
 
         Integer servicePort) {
+
+    public static AuthorityRouteRpcVoBuilder builder() {
+        return new AuthorityRouteRpcVoBuilder();
+    }
+
+    public static class AuthorityRouteRpcVoBuilder {
+        private String serviceHost;
+        private Integer servicePort;
+
+        public AuthorityRouteRpcVoBuilder serviceHost(String serviceHost) {
+            this.serviceHost = serviceHost;
+            return this;
+        }
+
+        public AuthorityRouteRpcVoBuilder servicePort(Integer servicePort) {
+            this.servicePort = servicePort;
+            return this;
+        }
+
+        public AuthorityRouteRpcVo build() {
+            return new AuthorityRouteRpcVo(serviceHost, servicePort);
+        }
+    }
 }
