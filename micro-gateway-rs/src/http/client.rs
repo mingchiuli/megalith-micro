514eabef9c0593de79e49a6fb378be3e5bb5b48b
mingchiuli <mingchiuli@163.com>
2025-02-23 03:18:21 +0800

    opt code

--- micro-gateway-rs/src/http/client.rs
+++ micro-gateway-rs/src/http/client.rs
@@ -6,10 +6,10 @@ use axum::{
     http::{request::Builder, HeaderName, HeaderValue},
     BoxError,
 };
-use http_body_util::BodyExt;
+use http_body_util::{combinators::BoxBody, BodyExt};
 use http_body_util::{Empty, Full};
 use hyper::{
-    body::Buf,
+    body::{Body, Buf},
     client::conn::http1::{self, SendRequest},
     Method, Response,
 };
@@ -19,13 +19,132 @@ use tokio::net::TcpStream;
 
 use crate::{exception::error::ClientError, util::http_util::set_headers};
 
-/// Helper function to create a new client connection
+async fn parse_req(
+    url: hyper::Uri,
+    method: Method,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<Builder, ClientError> {
+    let host = parse_host(&url)?;
+
+    let mut req = Request::builder()
+        .method(method)
+        .uri(url)
+        .header(hyper::header::HOST, host);
+    req = set_headers(req, headers);
+    Ok(req)
+}
+
+// Raw GET 方法
+pub async fn get_raw(
+    url: hyper::Uri,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<Response<Bytes>, BoxError> {
+    request_raw(Method::GET, url, None, headers).await
+}
+
+// Raw POST 方法
+pub async fn post_raw(
+    url: hyper::Uri,
+    body: axum::body::Body,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<Response<Bytes>, BoxError> {
+    request_raw(Method::POST, url, Some(body), headers).await
+}
+
+pub async fn get<T: DeserializeOwned>(
+    url: hyper::Uri,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<T, BoxError> {
+    request(Method::GET, url, None::<()>, headers).await
+}
+
+pub async fn post<T: DeserializeOwned>(
+    url: hyper::Uri,
+    body: impl Serialize,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<T, BoxError> {
+    request(Method::POST, url, Some(body), headers).await
+}
+
+fn parse_host(url: &hyper::Uri) -> Result<HeaderValue, ClientError> {
+    let host = url
+        .authority()
+        .ok_or(ClientError::Request("No authority found".to_string()))?
+        .as_str();
+
+    HeaderValue::from_str(host).map_err(|_| ClientError::Request("Invalid host".to_string()))
+}
+
+async fn parse_response<T>(response: Response<Bytes>) -> Result<T, BoxError>
+where
+    T: DeserializeOwned,
+{
+    let status = response.status();
+    let body = response.into_body();
+
+    // Check if status is success (2xx range)
+    if !status.is_success() {
+        return Err(Box::new(ClientError::Api(format!(
+            "Request failed with status: {}",
+            status
+        ))));
+    }
+
+    // Try to deserialize the response body
+    Ok(serde_json::from_reader(body.reader())
+        .map_err(|e| ClientError::Deserialize(e.to_string()))?)
+}
+
+pub async fn request<T: DeserializeOwned>(
+    method: Method,
+    url: hyper::Uri,
+    body: Option<impl Serialize>,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<T, BoxError> {
+    let body = body.map(|b| {
+        let json = serde_json::to_string(&b)
+            .map_err(|e| ClientError::Serialization(e.to_string()))
+            .unwrap();
+        axum::body::Body::from(json)
+    });
+
+    let response = request_raw(method, url, body, headers).await?;
+    parse_response(response).await
+}
+
+pub async fn request_raw(
+    method: Method,
+    url: hyper::Uri,
+    body: Option<axum::body::Body>,
+    headers: HashMap<HeaderName, HeaderValue>,
+) -> Result<Response<Bytes>, BoxError> {
+    let sender = create_connection::<BoxBody<Bytes, BoxError>>(&url).await?;
+    let req = parse_req(url, method, headers).await?;
+
+    let req = match body {
+        Some(body) => {
+            let body_bytes = body::to_bytes(body.into(), usize::MAX).await?;
+            let body = Full::new(body_bytes)
+                .map_err(|e| Box::new(ClientError::Request(e.to_string())) as BoxError)
+                .boxed();
+            req.body(body)
+        }
+        None => {
+            let body = Empty::<Bytes>::new()
+                .map_err(|e| Box::new(ClientError::Request(e.to_string())) as BoxError)
+                .boxed();
+            req.body(body)
+        }
+    }
+    .map_err(|e| ClientError::Request(e.to_string()))?;
+
+    let res_body = invoke(sender, req).await?;
+    Ok(Response::new(res_body))
+}
 
 async fn create_connection<B>(url: &hyper::Uri) -> Result<SendRequest<B>, BoxError>
 where
-    B: hyper::body::Body + Send + 'static,
-    B::Data: Send,
-    B::Error: Into<BoxError>,
+    B: Body<Data = Bytes, Error = BoxError> + Send + 'static,
 {
     let host = url
         .host()
@@ -50,118 +169,13 @@ where
     Ok(sender)
 }
 
-pub async fn get_raw(
-    url: hyper::Uri,
-    headers: HashMap<HeaderName, HeaderValue>,
-) -> Result<Response<Bytes>, BoxError> {
-    let sender = create_connection::<Empty<Bytes>>(&url).await?;
-
-    let req = parse_req(url, Method::GET, headers).await?;
-
-    let req = req
-        .body(Empty::<Bytes>::new())
-        .map_err(|e| ClientError::Request(e.to_string()))?;
-
-    let res_body = invoke(sender, req).await?;
-
-    Ok(Response::new(res_body))
-}
-
-async fn parse_req(
-    url: hyper::Uri,
-    method: Method,
-    headers: HashMap<HeaderName, HeaderValue>,
-) -> Result<Builder, ClientError> {
-    let host = parse_host(&url)?;
-
-    let mut req = Request::builder()
-        .method(method)
-        .uri(url)
-        .header(hyper::header::HOST, host);
-    req = set_headers(req, headers);
-    Ok(req)
-}
-
 async fn invoke<B>(mut sender: SendRequest<B>, req: Request<B>) -> Result<Bytes, BoxError>
 where
-    B: hyper::body::Body + Send + 'static,
+    B: Body + Send + 'static,
     B::Data: Send,
     B::Error: Into<BoxError>,
 {
-    let web_res = sender.send_request(req).await?;
-    Ok(web_res.into_body().collect().await?.to_bytes())
-}
-
-pub async fn get<T: DeserializeOwned>(
-    url: hyper::Uri,
-    headers: HashMap<HeaderName, HeaderValue>,
-) -> Result<T, BoxError> {
-    let resp = get_raw(url, headers).await?;
-    let data: T = parse_response(resp).await?;
-    Ok(data)
-}
-
-fn parse_host(url: &hyper::Uri) -> Result<HeaderValue, ClientError> {
-    let host = url
-        .authority()
-        .ok_or(ClientError::Request("No authority found".to_string()))?
-        .as_str();
-
-    HeaderValue::from_str(host).map_err(|_| ClientError::Request("Invalid host".to_string()))
-}
-
-pub async fn post_raw(
-    url: hyper::Uri,
-    req_body: axum::body::Body,
-    headers: HashMap<HeaderName, HeaderValue>,
-) -> Result<Response<Bytes>, BoxError> {
-    let sender = create_connection::<Full<Bytes>>(&url).await?;
-
-    let req = parse_req(url, Method::POST, headers).await?;
-
-    let body_bytes = body::to_bytes(req_body.into(), usize::MAX).await?;
-
-    let req = req
-        .body(Full::new(Bytes::from(body_bytes)))
-        .map_err(|e| ClientError::Request(e.to_string()))?;
-
-    let res_body = invoke(sender, req).await?;
-
-    Ok(Response::new(res_body))
-}
-
-pub async fn post<T: DeserializeOwned>(
-    url: hyper::Uri,
-    req_body: impl Serialize,
-    headers: HashMap<HeaderName, HeaderValue>,
-) -> Result<T, BoxError> {
-    // Serialize request body and set content type
-    let req_body =
-        serde_json::to_string(&req_body).map_err(|e| ClientError::Serialization(e.to_string()))?;
-
-    // Use post_raw to handle the request
-    let response = post_raw(url, axum::body::Body::from(req_body), headers).await?;
-
-    // Parse the response
-    parse_response(response).await
-}
-
-async fn parse_response<T>(response: Response<Bytes>) -> Result<T, BoxError>
-where
-    T: DeserializeOwned,
-{
-    let status = response.status();
-    let body = response.into_body();
-
-    // Check if status is success (2xx range)
-    if !status.is_success() {
-        return Err(Box::new(ClientError::Api(format!(
-            "Request failed with status: {}",
-            status
-        ))));
-    }
-
-    // Try to deserialize the response body
-    Ok(serde_json::from_reader(body.reader())
-        .map_err(|e| ClientError::Deserialize(e.to_string()))?)
+    let res = sender.send_request(req).await?;
+    let body = res.into_body().collect().await?.to_bytes();
+    Ok(body)
 }
