dcec400cea12b4f4f6420670b9cb74ccd41187bf
mingchiuli <mingchiuli@163.com>
2025-02-23 23:50:18 +0800

    fix npe

--- micro-blog/src/main/java/wiki/chiu/micro/blog/convertor/BlogSysCountSearchReqConvertor.java
+++ micro-blog/src/main/java/wiki/chiu/micro/blog/convertor/BlogSysCountSearchReqConvertor.java
@@ -4,12 +4,13 @@ import wiki.chiu.micro.blog.req.BlogDownloadReq;
 import wiki.chiu.micro.common.req.BlogSysCountSearchReq;
 
 public class BlogSysCountSearchReqConvertor {
-    public static BlogSysCountSearchReq convert(BlogDownloadReq downloadReq) {
+    public static BlogSysCountSearchReq convert(BlogDownloadReq downloadReq, Long userId) {
         return BlogSysCountSearchReq.builder()
                 .keywords(downloadReq.keywords())
                 .status(downloadReq.status())
                 .createEnd(downloadReq.createEnd())
                 .createStart(downloadReq.createStart())
+                .userId(userId)
                 .build();
     }
 }
