75fce225e1500e8048b2270996315178004c9e3f
mingchiuli <mingchiuli@163.com>
2025-04-25 20:03:58 +0800

    Update build configurations for all microservices

--- micro-search/build.gradle.kts
+++ micro-search/build.gradle.kts
@@ -32,6 +32,9 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 				-O3
 				-J-XX:MaxRAMPercentage=80.0
 				-H:+CompactingOldGen
+                -H:+TrackPrimitiveValues
+                -H:+UsePredicates
+                -XX:+UseCompactObjectHeaders
 			""",
                     "BP_HEALTH_CHECKER_ENABLED" to "true"
             )
