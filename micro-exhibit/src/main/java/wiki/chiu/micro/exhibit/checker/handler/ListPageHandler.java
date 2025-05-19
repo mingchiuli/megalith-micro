a58ff36900f75493d09c732022f35439acae93f8
mingchiuli <mingchiuli@163.com>
2025-05-20 00:36:00 +0800

    Check bloom filter existence before accessing bits
    Fix order of operations to verify the bloom filter exists before
    attempting to get individual bit values. This prevents errors when the
    filter is not yet initialized.

--- micro-exhibit/src/main/java/wiki/chiu/micro/exhibit/checker/handler/ListPageHandler.java
+++ micro-exhibit/src/main/java/wiki/chiu/micro/exhibit/checker/handler/ListPageHandler.java
@@ -1,5 +1,6 @@
 package wiki.chiu.micro.exhibit.checker.handler;
 
+import org.redisson.api.RBitSet;
 import wiki.chiu.micro.cache.handler.CheckerHandler;
 import wiki.chiu.micro.common.exception.MissException;
 import wiki.chiu.micro.common.lang.Const;
@@ -19,8 +20,14 @@ public class ListPageHandler extends CheckerHandler {
 
     @Override
     public void handle(Object[] args) {
+
+        RBitSet bitSet = redissonClient.getBitSet(Const.BLOOM_FILTER_PAGE);
+        if (!bitSet.isExists()) {
+            return;
+        }
+
         Integer currentPage = (Integer) args[0];
-        boolean bit = redissonClient.getBitSet(Const.BLOOM_FILTER_PAGE).get(currentPage);
+        boolean bit = bitSet.get(currentPage);
         if (!bit) {
             throw new MissException(NO_FOUND.getMsg() + currentPage + " page");
         }
