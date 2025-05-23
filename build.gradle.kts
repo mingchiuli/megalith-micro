427086e61da4bb6eaba3958de83b2b99fd60eb6b
mingchiuli <mingchiuli@163.com>
2025-05-23 08:58:27 +0800

    Bump version to 3.5.0 and remove Caffeine dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.6"
+    id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.6"
 }
@@ -19,7 +19,6 @@ subprojects {
 
     dependencyManagement {
         dependencies {
-            dependency("com.github.ben-manes.caffeine:caffeine:3.2.0")
             dependency("org.redisson:redisson:3.47.0")
             dependency("com.nimbusds:nimbus-jose-jwt:10.3")
             dependency("wiki.chiu.megalith:cache-spring-boot-starter:3.4.5.2")
