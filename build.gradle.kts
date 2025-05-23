2c262b126c6962e851452fb70ad48324459486d7
chiu <80095372+mingchiuli@users.noreply.github.com>
2025-05-23 15:59:04 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ subprojects {
             dependency("org.redisson:redisson:3.47.0")
             dependency("com.nimbusds:nimbus-jose-jwt:10.3")
             dependency("wiki.chiu.megalith:cache-spring-boot-starter:3.5.0")
+            dependency("com.github.ben-manes.caffeine:caffeine:3.2.0")
         }
     }
 
