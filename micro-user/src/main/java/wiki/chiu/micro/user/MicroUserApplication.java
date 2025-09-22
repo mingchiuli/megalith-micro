1cccac4f07e2fe9b2f7ed4b1a1ad156560b93171
mingchiuli <mingchiuli@163.com>
2025-09-22 22:22:26 +0800

    Make main method static
    The Spring Boot application requires a static main method.

--- micro-user/src/main/java/wiki/chiu/micro/user/MicroUserApplication.java
+++ micro-user/src/main/java/wiki/chiu/micro/user/MicroUserApplication.java
@@ -11,7 +11,7 @@ import wiki.chiu.micro.user.config.CustomRuntimeHints;
 @ImportRuntimeHints({ CustomRuntimeHints.class })
 public class MicroUserApplication {
 
-   void main(String[] args) {
+   static void main(String[] args) {
         SpringApplication.run(MicroUserApplication.class, args);
     }
 }
