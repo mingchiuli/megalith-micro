1e8b24eb3513bb318faa01e1be563d9fa4229504
mingchiuli <mingchiuli@163.com>
2025-09-22 10:30:12 +0800

    Remove static modifier from main methods
    This change removes the `static` modifier from the `main` methods of all
    microservice applications. This is done for consistency and to align
    with modern Java best practices.

--- micro-search/src/main/java/wiki/chiu/micro/search/MicroSearchApplication.java
+++ micro-search/src/main/java/wiki/chiu/micro/search/MicroSearchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(proxyBeanMethods = false)
 public class MicroSearchApplication {
 
-	public static void main(String[] args) {
+	void main(String[] args) {
 		SpringApplication.run(MicroSearchApplication.class, args);
 	}
 
