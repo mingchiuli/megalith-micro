e7523de12ea133798738792ee8ee35654d6e4207
mingchiuli <mingchiuli@163.com>
2025-11-01 01:32:53 +0800

    Fix: Handle login errors gracefully
    The previous implementation used a `finally` block which always set
    `loginLoading.value` to `false`, even if the login request errored. This
    change uses a `catch` block to only set it to `false` on error, allowing
    error handling to occur.

--- megalith-frontend/src/views/LoginView.vue
+++ megalith-frontend/src/views/LoginView.vue
@@ -22,7 +22,7 @@ const triggerSubmitLogin = async (username: string, password: string) => {
   try {
     loginLoading.value = true
     await submitLogin(username, password)
-  } finally {
+  } catch {
     loginLoading.value = false
   }
 }
