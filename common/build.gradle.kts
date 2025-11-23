eda0b7f1d4f705e0abf81873f05f8f3146fe8e51
mingchiuli <mingchiuli@163.com>
2025-11-23 15:02:00 +0800

    Switch to Spring MVC and add Micrometer tracing

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,11 +1,8 @@
 dependencies {
-    compileOnly("org.springframework:spring-web")
     compileOnly("org.springframework:spring-webmvc")
-    compileOnly("org.springframework:spring-context")
     compileOnly("org.springframework.boot:spring-boot-autoconfigure")
     compileOnly("tools.jackson.core:jackson-databind")
     compileOnly("jakarta.servlet:jakarta.servlet-api")
-    compileOnly("jakarta.annotation:jakarta.annotation-api")
     compileOnly("io.micrometer:micrometer-tracing-bridge-brave")
     compileOnly("io.zipkin.brave:brave-instrumentation-spring-web")
 }
--- micro-auth/build.gradle.kts
+++ micro-auth/build.gradle.kts
@@ -3,15 +3,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("org.springframework.boot:spring-boot-micrometer-tracing")
     implementation("io.zipkin.brave:brave-instrumentation-spring-web")
     implementation("org.redisson:redisson")
     implementation("com.nimbusds:nimbus-jose-jwt")
     implementation(project(":common"))
     implementation("wiki.chiu.megalith:cache-spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
 }
-
