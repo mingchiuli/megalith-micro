25a90da216ddd6b1fe50335cd56d0efdee2dbc92
mingchiuli <mingchiuli@163.com>
2025-11-25 00:31:26 +0800

    Add HttpService imports for OSS and Auth groups

--- micro-user/src/main/java/wiki/chiu/micro/user/MicroUserApplication.java
+++ micro-user/src/main/java/wiki/chiu/micro/user/MicroUserApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.service.registry.ImportHttpServices;
+import wiki.chiu.micro.common.rpc.AuthHttpService;
+import wiki.chiu.micro.common.rpc.OssHttpService;
 import wiki.chiu.micro.user.config.CustomRuntimeHints;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @EnableJpaAuditing
 @ImportRuntimeHints({ CustomRuntimeHints.class })
+@ImportHttpServices(group = "oss", types = {OssHttpService.class})
+@ImportHttpServices(group = "auth", types = {AuthHttpService.class})
 public class MicroUserApplication {
 
    public static void main(String[] args) {
