c832397f3129e1875e32fcd2d4287a058c08e6f2
mingchiuli <mingchiuli@163.com>
2025-11-30 18:05:16 +0800

    Add runtime hints for modules
    Register reflection types and resources for common, auth, blog, exhibit

---
+++ micro-auth/src/main/java/wiki/chiu/micro/auth/config/CustomRuntimeHints.java
@@ -0,0 +1,31 @@
+package wiki.chiu.micro.auth.config;
+
+import wiki.chiu.micro.auth.dto.ButtonDto;
+import wiki.chiu.micro.auth.dto.MenuDto;
+import wiki.chiu.micro.auth.dto.MenusAndButtonsDto;
+import wiki.chiu.micro.auth.vo.LoginSuccessVo;
+import wiki.chiu.micro.auth.vo.UserInfoVo;
+import org.springframework.aot.hint.*;
+
+import wiki.chiu.micro.common.vo.AuthorityRpcVo;
+
+
+public class CustomRuntimeHints implements RuntimeHintsRegistrar {
+
+
+    @Override// Register method for reflection
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        // Register method for reflection
+
+        hints.reflection()
+                .registerType(LoginSuccessVo.class)
+                .registerType(UserInfoVo.class)
+                .registerType(MenusAndButtonsDto.class)
+                .registerType(AuthorityRpcVo.class)
+                .registerType(MenuDto.class)
+                .registerType(ButtonDto.class);
+
+        hints.resources()
+                .registerPattern("logback-spring.xml");
+    }
+}
