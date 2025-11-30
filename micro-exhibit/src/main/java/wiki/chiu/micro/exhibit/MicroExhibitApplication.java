552e555f16e27024ce6062ff2fb1f082e6f5e67a
mingchiuli <mingchiuli@163.com>
2025-11-30 17:33:21 +0800

    Remove CustomRuntimeHints and tidy runtime hints
    Drop per-module CustomRuntimeHints and simplify CommonRuntimeHints to
    register only Lua scripts and remove logback registrations.

--- micro-exhibit/src/main/java/wiki/chiu/micro/exhibit/MicroExhibitApplication.java
+++ micro-exhibit/src/main/java/wiki/chiu/micro/exhibit/MicroExhibitApplication.java
@@ -2,15 +2,12 @@ package wiki.chiu.micro.exhibit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.service.registry.ImportHttpServices;
 import wiki.chiu.micro.common.rpc.*;
-import wiki.chiu.micro.exhibit.config.CustomRuntimeHints;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @EnableScheduling
-@ImportRuntimeHints({ CustomRuntimeHints.class })
 @ImportHttpServices(group = "user", types = {UserHttpService.class})
 @ImportHttpServices(group = "blog", types = {BlogHttpService.class})
 @ImportHttpServices(group = "auth", types = {AuthHttpService.class})
