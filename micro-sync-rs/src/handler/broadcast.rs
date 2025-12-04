3068fd54575388f2760dc5d6556d5b7d237ffc4a
mingchiuli <mingchiuli@163.com>
2025-12-04 16:45:23 +0800

    Add log

--- micro-sync-rs/src/handler/broadcast.rs
+++ micro-sync-rs/src/handler/broadcast.rs
@@ -27,6 +27,7 @@ pub async fn ws_handler(
     room_manager: Arc<Mutex<RoomManager>>,
 ) -> Result<impl Reply, Rejection> {
     // 从请求头中提取 trace context
+    tracing::info!("header: {:?}", headers);
     let parent_context = global::get_text_map_propagator(|propagator| {
         propagator.extract(&WarpHeaderExtractor(&headers))
     });
