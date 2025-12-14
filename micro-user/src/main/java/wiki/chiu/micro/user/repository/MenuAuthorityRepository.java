dbf18840e42e4f2ff31245f4f7bb85fbc97aa6b0
mingchiuli <mingchiuli@163.com>
2025-12-14 18:09:52 +0800

    Add transactional annotations to repository methods
    This commit adds `@Transactional` annotations to several methods in the
    `MenuAuthorityRepository`, `RoleMenuRepository`, and
    `UserRoleRepository` interfaces. This ensures that these database
    operations are executed within a transaction.

--- micro-user/src/main/java/wiki/chiu/micro/user/repository/MenuAuthorityRepository.java
+++ micro-user/src/main/java/wiki/chiu/micro/user/repository/MenuAuthorityRepository.java
@@ -2,17 +2,21 @@ package wiki.chiu.micro.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.transaction.annotation.Transactional;
 import wiki.chiu.micro.user.entity.MenuAuthorityEntity;
 
 import java.util.List;
 
 public interface MenuAuthorityRepository extends JpaRepository<MenuAuthorityEntity, Long> {
 
+    @Transactional
     void deleteByMenuId(Long menuId);
 
     List<MenuAuthorityEntity> findByMenuId(Long menuId);
 
+    @Transactional
     void deleteByAuthorityIdIn(List<Long> ids);
 
+    @Transactional
     void deleteByAuthorityId(Long authorityId);
 }
