b4c5846ef3aab2f7c56cd9d1bfafb1382128bdd7
mingchiuli <mingchiuli@163.com>
2025-12-14 12:10:46 +0800

    Remove unused methods from ApiResult
    These methods were providing access to fields that aren't generally
    useful to consumers of the ApiResult struct.

--- micro-gateway-rs/src/result/api_result.rs
+++ micro-gateway-rs/src/result/api_result.rs
@@ -2,31 +2,19 @@ use serde::Deserialize;
 
 // Custom error type for auth-related errors
 #[derive(Deserialize, Debug)]
+
 pub struct ApiResult<T> {
     code: i32,
+    #[allow(dead_code)]
     msg: String,
     data: T,
 }
 
 impl<T> ApiResult<T> {
-    /// Creates a new response
-    pub fn new(code: i32, msg: String, data: T) -> Self {
-        Self { code, msg, data }
-    }
-
-    /// Returns a reference to the response data
-    pub fn data(&self) -> &T {
-        &self.data
-    }
 
     /// Returns the response code
     pub fn code(&self) -> i32 {
         self.code
-    }
-
-    /// Returns a reference to the response message
-    pub fn msg(&self) -> &str {
-        &self.msg
     }
 
     /// Consumes the response and returns the data
@@ -34,8 +22,4 @@ impl<T> ApiResult<T> {
         self.data
     }
 
-    /// Consumes the response and returns all fields
-    pub fn into_inner(self) -> (i32, String, T) {
-        (self.code, self.msg, self.data)
-    }
 }
