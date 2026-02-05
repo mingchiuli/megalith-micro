850c579156badbb32011a232c095e3b91015372a
mingchiuli <mingchiuli@163.com>
2026-02-05 22:31:00 +0800

    Update Redisson dependency to version 4.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,7 +112,7 @@ subprojects {
 
     configure<DependencyManagementExtension> {
         dependencies {
-            dependency("org.redisson:redisson:4.1.0")
+            dependency("org.redisson:redisson:4.2.0")
             dependency("com.nimbusds:nimbus-jose-jwt:10.7")
             dependency("wiki.chiu.megalith:cache-spring-boot-starter:4.0.3")
         }
