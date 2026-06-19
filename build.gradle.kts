cfdd95791ad63f886c8d8f06ad1a4cc878a2311d
chiu <80095372+mingchiuli@users.noreply.github.com>
2026-06-20 02:29:27 +0800

    Update cache-spring-boot-starter dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,7 +111,7 @@ subprojects {
         dependencies {
             dependency("org.redisson:redisson:4.6.1")
             dependency("com.nimbusds:nimbus-jose-jwt:10.9.1")
-            dependency("wiki.chiu.megalith:cache-spring-boot-starter:4.6.0")
+            dependency("wiki.chiu.megalith:cache-spring-boot-starter:4.6.1")
         }
     }
 
