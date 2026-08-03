736d6b19ad317f7afc06413727fa9c1a0d5b1665
mingchiuli <mingchiuli@163.com>
2026-08-03 08:56:55 +0800

    fix: conditionally configure functional validation

--- common/src/main/java/wiki/chiu/micro/common/web/FunctionalWebValidationAutoConfiguration.java
+++ common/src/main/java/wiki/chiu/micro/common/web/FunctionalWebValidationAutoConfiguration.java
@@ -2,12 +2,16 @@ package wiki.chiu.micro.common.web;
 
 import jakarta.validation.Validator;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
+@AutoConfigureAfter(name = "org.springframework.boot.validation.autoconfigure.ValidationAutoConfiguration")
 @ConditionalOnClass(Validator.class)
+@ConditionalOnBean(Validator.class)
 public class FunctionalWebValidationAutoConfiguration {
 
     @Bean
