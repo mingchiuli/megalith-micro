15b50472f14945f4960780af3cf6f03a5461ec1f
mingchiuli <mingchiuli@163.com>
2026-08-20 17:17:38 +0800

    fix: restore native backend menu collapse

--- megalith-frontend/src/components/sys/InfiniteMenuItem.vue
+++ megalith-frontend/src/components/sys/InfiniteMenuItem.vue
@@ -7,10 +7,6 @@ import * as ElementPlusIcons from '@element-plus/icons-vue'
 const router = useRouter()
 const props = defineProps<{
   item: Menu
-  collapsed: boolean
-}>()
-const emit = defineEmits<{
-  openCatalogue: [index: string]
 }>()
 
 const isRouteMenuNode = (node: MenuNode): node is Menu => node.type !== RoutesEnum.BUTTON
@@ -21,12 +17,6 @@ const resolveMenuIcon = (name: string) =>
 const selectMenu = (item: Tab) => {
   tabStore().addTab(item)
   router.push({ name: item.name })
-}
-
-const openCollapsedCatalogue = (event: MouseEvent, index: string) => {
-  if (!props.collapsed) return
-  event.stopPropagation()
-  emit('openCatalogue', index)
 }
 </script>
 
@@ -44,29 +34,12 @@ const openCollapsedCatalogue = (event: MouseEvent, index: string) => {
   <!-- 有子节点，使用 el-sub-menu 渲染 -->
   <el-sub-menu v-else-if="item.type === RoutesEnum.CATALOGUE" :index="String(item.id)">
     <template #title>
-      <div class="catalogue-title" @click="openCollapsedCatalogue($event, String(item.id))">
-        <el-icon :size="20">
-          <component :is="resolveMenuIcon(item.icon)" />
-        </el-icon>
-        <span>{{ item.title }}</span>
-      </div>
+      <el-icon :size="20">
+        <component :is="resolveMenuIcon(item.icon)" />
+      </el-icon>
+      <span>{{ item.title }}</span>
     </template>
     <!-- 递归渲染 -->
-    <InfiniteMenuItem
-      v-for="sub in visibleChildren"
-      v-bind:key="sub.id"
-      :item="sub"
-      :collapsed="collapsed"
-      @open-catalogue="emit('openCatalogue', $event)"
-    />
+    <InfiniteMenuItem v-for="sub in visibleChildren" v-bind:key="sub.id" :item="sub" />
   </el-sub-menu>
 </template>
-
-<style scoped>
-.catalogue-title {
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-}
-</style>
