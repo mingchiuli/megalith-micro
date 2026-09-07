a6e57f0f3b558f032bbd2c8345b885cd79888664
mingchiuli <mingchiuli@163.com>
2026-09-08 01:21:48 +0800

    chore: bump GraalVM Native buildtools and remove unused SSL hints
    Update the org.graalvm.buildtools.native plugin to version 1.1.12 and
    remove the now-unused SSLParameters and H2TlsSupport reflection hints
    from
    CommonRuntimeHints, as they are no longer required.

--- common-observability/src/main/java/wiki/chiu/micro/common/aot/hints/CommonRuntimeHints.java
+++ common-observability/src/main/java/wiki/chiu/micro/common/aot/hints/CommonRuntimeHints.java
@@ -2,12 +2,9 @@ package wiki.chiu.micro.common.aot.hints;
 
 import java.util.List;
 
-import javax.net.ssl.SSLParameters;
-
 import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.TypeReference;
 
 class CommonRuntimeHints implements RuntimeHintsRegistrar {
 
@@ -21,17 +18,5 @@ class CommonRuntimeHints implements RuntimeHintsRegistrar {
                 classLoader,
                 "com.google.protobuf.ExtensionRegistry",
                 typeHint -> typeHint.withMethod("getEmptyRegistry", List.of(), ExecutableMode.INVOKE));
-
-        hints
-            .reflection()
-            .registerType(
-                SSLParameters.class,
-                typeHint ->
-                    typeHint
-                        .onReachableType(TypeReference.of("org.apache.hc.core5.http2.ssl.H2TlsSupport"))
-                        .withMethod(
-                            "setEnableRetransmissions",
-                            TypeReference.listOf(boolean.class),
-                            ExecutableMode.INVOKE));
     }
 }
