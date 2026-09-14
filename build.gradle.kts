d43eea7fb1f065490b2c9105669ee062ad73b3d5
mingchiuli <mingchiuli@163.com>
2026-09-15 01:41:45 +0800

    build: downgrade GraalVM native plugin to 1.1.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     // Only declare plugin versions, don't apply to root project
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("org.springframework.boot") version "4.1.1" apply false
-    id("org.graalvm.buildtools.native") version "1.1.13" apply false
+    id("org.graalvm.buildtools.native") version "1.1.12" apply false
     id("org.hibernate.orm") version "7.4.7.Final" apply false
 }
 
